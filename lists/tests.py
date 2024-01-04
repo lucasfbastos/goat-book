24480143b16ba1e0eee96ea98d39ea65f4bb176d
Lucas Bastos <lucas.fbastos@gmail.com>
2024-01-03 22:54:16 -0300

    Namespaced lists app templates

--- lists/tests.py
+++ lists/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "lists/home.html")
